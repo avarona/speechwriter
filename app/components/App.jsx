2887552c1fcf081400478fdc692ea45c5d662572
avarona <avarona10@gmail.com>
2017-03-10 14:20:19 -0500

    feat(speech): add voice synthesis funcitonality and keyboard typing

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Home from './Home';
 
-/* The 'App' component is the parent of all other components in React-Router. */
-
 const App = props => {
 
   return (
--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import annyang from 'annyang';
+import Artyom from 'artyom.js';
 
-// The Home component might contain your homepage content. Adding new routes to routes.js will cause them to be rendered instead when those urls are accessed.
+let artyom = Artyom.ArtyomBuilder.getInstance();
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {text: ''}
-    this.record = this.record.bind(this)
+    this.voiceRecord = this.voiceRecord.bind(this)
+    this.keyboardType = this.keyboardType.bind(this)
+    this.speechSynthesis = this.speechSynthesis.bind(this)
+  }
+
+  componentDidMount() {
+    return this.voiceRecord()
   }
 
   render() {
@@ -16,37 +22,95 @@ class Home extends Component {
       <div>
         <h2>Speech to Text</h2>
         <div
-          id="cube"
-          onClick={this.record}>
-            <button>Click Me</button>
+          id="cube">
+            <div>
+              <textarea
+                className="main-doc"
+                type="text"
+                style={{width: '600px', height: '50vh'}}
+                placeholder="type or start talking..."
+                value={this.state.text}
+                onChange={this.keyboardType}
+              />
+            </div>
+            <div>
+              <button
+                className="btn btn-primary"
+                onClick={this.voiceRecord}>
+                  <i className="fa fa-microphone fa-2x" />
+              </button>
+              <button
+                className="btn btn-info"
+                onClick={this.speechSynthesis}>
+                  <i className="fa fa-volume-up fa-2x" />
+              </button>
+            </div>
         </div>
       </div>
     )
   }
 
-  record() {
-    let words = ''
+  keyboardType(evt) {
+    this.setState({text: evt.target.value})
+  }
+
+  voiceRecord() {
     if (annyang) {
-      console.log('annyang is listening')
+      console.log('annyang is enabled')
+      let words = this.state.text
 
-      annyang.addCallback('result', function(phrases) {
+    // TODO: FIX voice recognition overwrites typed state
+
+    // TODO: CREATE erase all ; enter ; indent ; show me timer ; submit/save
+      const commands = {
+        'erase all': () => {
+          this.setState({text: ''})
+          console.log('erase all command hit', this.state)
+        },
+        'enter': () => {
+          words = words + '\n'
+          console.log('enter command hit', words)
+      },
+        'indent': () => {
+          words = '  ' + words
+          console.log('indent command hit', words)
+      },
+        'show me timer': () => {
+          console.log('show me timer command hit')
+      },
+        'save': () => {
+          console.log('save command hit', words)
+        }
+      }
+
+      annyang.addCallback('result', (phrases) => {
         words += phrases[0]
-        console.log("I think the user said: ", phrases[0]);
-        console.log("But then again, it could be any of the following: ", phrases);
+        console.log('I think the user said: ', phrases[0]);
+        console.log('But then again, it could be any of the following: ', phrases);
+        this.setState({text: words})
       })
-
-      // Tell KITT to use annyang
-      SpeechKITT.annyang();
-
-      // Define a stylesheet for KITT to use
-      SpeechKITT.setStylesheet('//cdnjs.cloudflare.com/ajax/libs/SpeechKITT/0.3.0/themes/flat.css');
-
-      // Render KITT's interface
-      SpeechKITT.vroom();
-
     }
 
   }
+
+  speechSynthesis() {
+    if (artyom) {
+      console.log('artyom is enabled')
+      if (!this.state.text) {
+        artyom.say('There is nothing in your document')
+      } else {
+        artyom.say(this.state.text, {
+          onStart: function(){
+            console.log('Text is being read...')
+          },
+          onEnd: function(){
+            console.log('End of text')
+          }
+        });
+      }
+    }
+  }
+
 }
 
 export default Home;
