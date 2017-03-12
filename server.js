4c042a9dedcc351eed6c28117947a79a78ba56b6
avarona <avarona10@gmail.com>
2017-03-11 19:09:38 -0500

    fix(server): server port fix for deployment

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.post('/api/docs', function(req, res, next) {
 app.get('/*', (req, res) => res.sendFile(resolve(__dirname, 'index.html')))
 
 // server listening!
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is listening on port', 3000);
   db.sync()
   .then(function() {
