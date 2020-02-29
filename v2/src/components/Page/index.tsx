e5e06079ba51e92a61c189e3834e6e1ff37d91e3
Alejandro Varona <avarona10@gmail.com>
2020-02-28 23:10:36 -0500

    refactor page component

--- v2/src/components/Page/index.tsx
+++ v2/src/components/Page/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import Button from '../Button';
+import PageTitle from '../PageTitle';
 import styles from './styles.module.scss';
 
 type Props = {
     placeholder: string;
+    title: string;
+    body: string;
+    handleTitleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    handlePageChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-const Page = ({ placeholder }: Props) => (
+const Page = ({ placeholder, title, body, handleTitleChange, handlePageChange }: Props) => (
     <div className={styles.pageContainer}>
+        <PageTitle onChange={handleTitleChange}>{title}</PageTitle>
+
         <textarea
             className={styles.textarea}
             placeholder={placeholder}
+            value={body}
+            onChange={handlePageChange}
             autoFocus
         />
     </div>
